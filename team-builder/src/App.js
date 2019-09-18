12a6dad745a35ab76f6c9df415d36a468640b149
Oluwa-femi <oluwatolablessing@gmail.com>
2019-09-18 15:29:10 +0100

    State properties and values declared

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import TeamForm from './Components/Form';
+import TeamList from './Components/TeamList';
+import data from './data'
 
 function App() {
+  const [members, setMembers] = useState(data);
+
+  const addNewPerson = person => {
+    setMembers([...members, person])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Web U3 Anna</h1>
+      <TeamList teamList={members} />
+      <Form addNewPerson={addNewPerson} />
     </div>
   );
 }
